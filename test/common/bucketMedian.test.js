6fb7cd1b73556a8b97cb9349a8ecf8eba381b657
Steve Gury <sgury@netflix.com>
2016-09-14 14:51:01 -0700

    WIP

--- test/common/bucketMedian.test.js
+++ test/common/bucketMedian.test.js
@@ -6,40 +6,50 @@ var expect = require('chai').expect;
 
 var getRandomInt = require('./getRandomInt');
 var BucketMedian = require('../../lib/common/bucketMedian');
+var Median = require('../../lib/common/median');
 
 describe('BucketMedian', function () {
 
     function measureError() {
-        var med = new BucketMedian(128);
+
+        var med = new BucketMedian();
+        var streaming = new Median();
         var buffer = [];
 
         for (var i = 0; i < 100; i++) {
-            var x = getRandomInt(1000, 10000);
+            var x = getRandomInt(10 * 1000, 100 * 1000);
             med.insert(x);
+            streaming.insert(x);
             buffer.push(x);
         }
 
         buffer.sort(function (a, b) { return a - b; });
         var expectedMedian = buffer[Math.floor(buffer.length / 2)];
         var estimatedMedian = med.estimate();
+        var streamingEstimation = streaming.estimation();
 
-        // console.log('expected: ' + expectedMedian + ', estimated: ' + estimatedMedian);
-        return Math.abs(estimatedMedian - expectedMedian) / expectedMedian;
+        // console.log('expected: ' + expectedMedian + ', estimated: ' + estimatedMedian + ', streaming: ' + streamingEstimation);
+        var error0 = Math.abs(estimatedMedian - expectedMedian) / expectedMedian;
+        var error1 = Math.abs(streamingEstimation - expectedMedian) / expectedMedian;
+        return [error0, error1];
     }
 
     it.only('works', function (done) {
-        this.timeout(21000);
+        this.timeout(30000);
 
-        var sum = 0;
-        var n = 500;
+        var sum0 = 0;
+        var sum1 = 0;
+        var n = 1000;
         for(var i = 0; i < n; i++) {
-            var error = measureError();
-            console.log(i + ': ' + error);
-            sum += error;
+            var errors = measureError();
+            console.log(i + ': ' + errors[0].toFixed(3) + ',\t' + errors[1].toFixed(3));
+            sum0 += errors[0];
+            sum1 += errors[1];
         }
-        var avg = sum / n;
-        console.log('avg: ' + avg);
-        expect(avg).to.be.within(0, 0.5);
+        var avg0 = sum0 / n;
+        var avg1 = sum1 / n;
+        console.log('avg0: ' + (100 * avg0).toFixed(2) + '%,\tavg1: ' + (100 * avg1).toFixed(2) + '%');
+        expect(avg0).to.be.within(0, 0.05);
         done();
     });
 });
