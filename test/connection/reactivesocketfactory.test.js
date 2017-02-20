2a7229fb6e6e8e21a8a52a36476828f546844f0d
Steve Gury <sgury@netflix.com>
2017-02-20 15:02:11 -0800

    Test passing the recorder from the factory.

--- test/connection/reactivesocketfactory.test.js
+++ test/connection/reactivesocketfactory.test.js
@@ -1,23 +1,36 @@
 'use strict';
 
+var assert = require('chai').assert;
+var metrix = require('metrix');
+
 var startEchoServer = require('../common/startEchoServer');
 var ReactiveSocketFactory =
     require('../../lib/connection/reactiveSocketFactory');
 
-describe('ReactiveSocketFactory', function () {
+describe.only('ReactiveSocketFactory', function () {
     it('Create a factory from ip:port', function (done) {
         this.timeout(30 * 1000);
+
+        var recorder = metrix.createRecorder();
+        var aggregator = metrix.createAggregator(recorder);
 
         var port = 8080;
         var server = startEchoServer({port: 8080, host: 'localhost'});
         var factory = new ReactiveSocketFactory({
             port: port,
-            host: 'localhost'
+            host: 'localhost',
+            recorder: recorder
         });
 
         factory.build().on('reactivesocket', function (rs) {
             rs.request({data: 'Hey'}).on('response', function (res) {
                 server.close();
+                var report = aggregator.report();
+
+                assert(report.counters['connection/requests']);
+                assert(report.counters['connection/responses']);
+                assert(report.histograms['connection/requestLatency']);
+                assert(report.histograms['connection/setupLatency']);
                 done();
             });
         });
